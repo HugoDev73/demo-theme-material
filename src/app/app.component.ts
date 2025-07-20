35607a57aa6cfd54808256e8a030d36352398c4b
hugo <hugo.dev73@gmail.com>
2025-07-20 13:22:50 -0600

    theme in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,41 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatStepperModule } from '@angular/material/stepper';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { ButtonsComponent } from './pages/buttons/buttons.component';
+import { CardContainerComponent } from './components/card-container/card-container.component';
+import { CheckboxComponent } from './components/checkbox/checkbox.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MATERIAL_MODULES } from './shared/material-modules';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, MatButtonModule],
+  imports: [
+    RouterModule,
+    ReactiveFormsModule,
+    ButtonsComponent,
+    CardContainerComponent,
+    CheckboxComponent,
+    ...MATERIAL_MODULES
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'demo-theme-material';
-/*   router = inject(Router)
-
-  onTabChange(event: MatTabChangeEvent) {
-    switch (event.index) {
-      case 0:
-        this.router.navigate(['/']);
-        break;
-      case 1:
-        this.router.navigate(['/buttons']);
-        break;
-      case 2:
-        this.router.navigate(['/forms']);
-        break;
-    }
-  } */
+  readonly bestBoys: string[] = [
+    'Samoyed',
+    'Akita Inu',
+    'Alaskan Malamute',
+    'Siberian Husky',
+  ];
 }
---
+++ src/app/shared/material-modules.ts
@@ -0,0 +1,56 @@
+// src/app/shared/material-imports.ts
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatStepperModule } from '@angular/material/stepper';
+
+export const MATERIAL_MODULES = [
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatDialogModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatListModule,
+  MatTabsModule,
+  MatTooltipModule,
+  MatSnackBarModule,
+  MatProgressSpinnerModule,
+  MatCheckboxModule,
+  MatRadioModule,
+  MatSlideToggleModule,
+  MatMenuModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatChipsModule,
+  MatButtonToggleModule,
+  MatStepperModule
+];
